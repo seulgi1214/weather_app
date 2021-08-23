dc4af985428cead40879f7efe3a1141b4b5df9f3
seulgi1214 <leeseulgi1214@gmail.com>
2021-08-23 10:30:29 +0900

    api가져와서 화면에 출력

--- README.md
+++ README.md
@@ -1,24 +1,8 @@
 # weather_app
 
-## Project setup
 ```
-npm install
-```
+날씨앱 study
+- weather api를 가져와서 화면에 날씨와 온도를 출력
+- 자바스크립트 fetch함수를 사용하여 api호출
 
-### Compiles and hot-reloads for development
 ```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template> 
-  <div id="app">
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'wram' : '' ">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search..."/>
+        <input type="text" class="search-bar" placeholder="Search..." v-model="query" @keypress="fetchWeather"/>
       </div>
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">Seoul, KOREA</div>
-          <div class="date">Sunday 22 August 2021 </div>
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
         <div class="weather-box">
-          <div class="temp">25°C</div>
-          <div class="weather">Rain</div>
+          <div class="temp">{{ Math.round(weather.main.temp) }}°C</div>
+          <div class="weather">{{weather.weather[0].main }}</div>
         </div>
       </div>
     </main>
@@ -26,9 +26,37 @@ export default {
   name: "App",
   data () {
     return {
-        api_key: '5ff80212d44ba22bf779f190b10f7327'
+        api_key: '5ff80212d44ba22bf779f190b10f7327',
+        url_base: "https://api.openweathermap.org/data/2.5/",
+        query: '',
+        weather: {}
     }
-  }
+  },
+  methods: {
+    fetchWeather(e) {
+      if(e.key == "Enter") {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      } 
+    },
+    setResults (results) {
+      this.weather = results;
+    },
+    dateBuilder () {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day =  days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`
+    }
+  },
 };
 </script>
 
@@ -45,11 +73,18 @@ export default {
   }
 
   #app {
-    background-image: url('./assets/night-bg.jpg');
+    background-image: url('./assets/cold.jpg');
     background-size: cover;
     background-position: top;
     transition: 0.4s;
+    max-width: 500px;
+    margin: 0 auto;
   }
+
+  #app.wram {
+    background-image: url('./assets/wram.jpg');
+  }
+
 
   main {
     min-height: 100vh;
